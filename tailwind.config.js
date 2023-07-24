27b11db730473e539a1cde8e6f94a7e2dd49d1cd
orianig <oriana.ig016@gmail.com>
2023-07-24 20:32:14 +0200

    feat: tailwind configuration

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors:{
+        primary: '#79B4B7',
+        secondary:{
+          100: '#edebeb',
+          500: '#EBA603',
+          900: '#E0DDDD'
+        }
+      }
+    },
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+  ],
+}
