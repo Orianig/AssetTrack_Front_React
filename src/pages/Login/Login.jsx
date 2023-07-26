c9dbf6b2d12c1edba6204c012ee553d8597282fd
orianig <oriana.ig016@gmail.com>
2023-07-26 18:30:38 +0200

    style: structure and components for login

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,4 +1,9 @@
 import React, { useEffect, useState } from "react";
+import Bg1 from "../../assets/images/login-1.jpg";
+import Bg2 from "../../assets/images/login-2.jpg";
+import Bg3 from "../../assets/images/login-3.jpg";
+import Bg4 from "../../assets/images/login-4.jpg";
+import Logo from "../../assets/images/logo-4.png";
 // import { login } from "../../services/auth.service";
 // import { login as loginStore } from "../../redux/slices/user.slice";
 // import { useNavigate } from "react-router-dom";
@@ -7,146 +12,130 @@ import React, { useEffect, useState } from "react";
 // import jwtDecode from "jwt-decode";
 
 const Login = () => {
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const images = [Bg1, Bg2, Bg3, Bg4];
 
-  // const handleBlur = (event) => {
-  //   if (event.target.name === 'email') {
-  //     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  //     if (!emailRegex.test(email)) {
-  //       setEmailError('Por favor, introduce un correo válido.');
-  //     } else {
-  //       setEmailError('');
-  //     }
-  //   } else if (event.target.name === 'password') {
-  //     const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
-  //     if (!passwordRegex.test(password)) {
-  //       setPasswordError('La contraseña debe tener al menos 8 caracteres, una mayúscula y un número.');
-  //     } else {
-  //       setPasswordError('');
-  //     }
-  //   }
-  // };
+  const handlePrevSlide = () => {
+    setCurrentSlide((prevSlide) =>
+      prevSlide === 0 ? images.length - 1 : prevSlide - 1
+    );
+  };
+
+  const handleNextSlide = () => {
+    setCurrentSlide((prevSlide) =>
+      prevSlide === images.length - 1 ? 0 : prevSlide + 1
+    );
+  };
 
   return (
     <>
-      <div className="h-screen w-screen bg-secondary">
-  <div className="h-screen w-screen container">
-    <div className="flex h-full flex-wrap items-center justify-center text-neutral-800">
-      <div className="w-full h-full">
-        <div className="block h-full bg-primary shadow-lg">
-          <div className="lg:flex lg:flex-wrap">
-                  {/* Right column container with background and description */}
-                  <div
-                    className="flex items-center rounded-b-lg lg:w-6/12 lg:rounded-r-lg lg:rounded-bl-none bg-primary"
-                  >
-                    <div class="px-4 py-6 text-white md:mx-6 md:p-12">
-                      <h4 class="mb-6 text-xl font-semibold">
-                        We are more than just a company
-                      </h4>
-                      <p class="text-sm">
-                        Lorem ipsum dolor sit amet, consectetur adipisicing
-                        elit, sed do eiusmod tempor incididunt ut labore et
-                        dolore magna aliqua. Ut enim ad minim veniam, quis
-                        nostrud exercitation ullamco laboris nisi ut aliquip ex
-                        ea commodo consequat.
-                      </p>
-                    </div>
-                  </div>
+      <div className="h-screen flex flex-col lg:flex-row">
+        {/* Left column container */}
 
-                  {/* Left column container */}
-                  <div className="px-4 md:px-0 lg:w-6/12">
-                    <div className="md:mx-6 md:p-12">
-                      {/* Logo */}
-                      <div className="text-center">
-                        <img
-                          className="mx-auto w-48"
-                          src="https://tecdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/lotus.webp"
-                          alt="logo"
-                        />
-                        <h4 className="mb-12 mt-1 pb-1 text-xl font-semibold">
-                          We are The Lotus Team
-                        </h4>
-                      </div>
+        {/* Carousel */}
 
-                      <form>
-                        <p className="mb-4">Please login to your account</p>
-                        {/* Email input */}
-                        <div className="relative">
-                          <input
-                            type="text"
-                            name="email"
-                            className="w-full px-3 py-2"
-                            // value={email}
-                            // onChange={(e) => setEmail(e.target.value)}
-                            // onBlur={handleBlur}
-                            // className={`w-full px-3 py-2 border-b-2 outline-none transition-all ${
-                            //   emailError
-                            //     ? "border-red-500"
-                            //     : "border-gray-500 focus:border-blue-500"
-                            // }`}
-                            placeholder="Correo electrónico"
-                          />
-                          {/* {emailError && (
-                            <span className="absolute bottom-0 left-0 text-red-500 text-sm">
-                              {emailError}
-                            </span>
-                          )} */}
-                        </div>
-
-                        {/* Password input */}
-                        <div
-                          className="relative mb-4"
-                          data-te-input-wrapper-init
-                        >
-                          <input
-                            type="password"
-                            className="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 data-[te-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:placeholder:text-neutral-200 [&:not([data-te-input-placeholder-active])]:placeholder:opacity-0"
-                            id="exampleFormControlInput11"
-                            placeholder="Password"
-                          />
-                          <label
-                            htmlFor="exampleFormControlInput11"
-                            className="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[1.6] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[0.9rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[te-input-state-active]:-translate-y-[0.9rem] peer-data-[te-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-200 dark:peer-focus:text-primary"
-                          >
-                            Password
-                          </label>
-                        </div>
-
-                        {/* Submit button */}
-                        <div className="mb-12 pb-1 pt-1 text-center">
-                          <button
-                            className="mb-3 inline-block w-full rounded px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-white shadow-[0_4px_9px_-4px_rgba(0,0,0,0.2)] transition duration-150 ease-in-out hover:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.1),0_4px_18px_0_rgba(0,0,0,0.2)] focus:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.1),0_4px_18px_0_rgba(0,0,0,0.2)] focus:outline-none focus:ring-0 active:shadow-[0_8px_9px_-4px_rgba(0,0,0,0.1),0_4px_18px_0_rgba(0,0,0,0.2)]"
-                            type="button"
-                            data-te-ripple-init
-                            data-te-ripple-color="light"
-                            style={{
-                              background:
-                                "linear-gradient(to right, #ee7724, #d8363a, #dd3675, #b44593)",
-                            }}
-                          >
-                            Log in
-                          </button>
-
-                          {/* Forgot password link */}
-                          <a href="#!">Forgot password?</a>
-                        </div>
-
-                        {/* Register button */}
-                        <div className="flex items-center justify-between pb-6">
-                          <p className="mb-0 mr-2">Don't have an account?</p>
-                          <button
-                            type="button"
-                            className="inline-block rounded border-2 border-danger px-6 pb-[6px] pt-2 text-xs font-medium uppercase leading-normal text-danger transition duration-150 ease-in-out hover:border-danger-600 hover:bg-neutral-500 hover:bg-opacity-10 hover:text-danger-600 focus:border-danger-600 focus:text-danger-600 focus:outline-none focus:ring-0 active:border-danger-700 active:text-danger-700 dark:hover:bg-neutral-100 dark:hover:bg-opacity-10"
-                            data-te-ripple-init
-                            data-te-ripple-color="light"
-                          >
-                            Register
-                          </button>
-                        </div>
-                      </form>
-                    </div>
-                  </div>
-                </div>
+        <div className="slider ">
+          <div className="h-screen relative overflow-hidde">
+            {images.map((imageUrl, index) => (
+              <div
+                key={index}
+                className={`${
+                  index === currentSlide ? "opacity-100" : "opacity-0"
+                } transition-opacity duration-700 ease-in-out absolute h-full w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2`}
+              >
+                <img
+                  src={imageUrl}
+                  className="block w-full h-full object-cover"
+                  alt={`Slide ${index + 1}`}
+                />
               </div>
+            ))}
+          </div>
+          {/* Slider controls */}
+          <button
+            type="button"
+            className="buttonLeft"
+            onClick={handlePrevSlide}
+          >
+            &lt;
+          </button>
+          <button
+            type="button"
+            className="buttonRight"
+            onClick={handleNextSlide}
+          >
+            &gt;
+          </button>
+          {/* Slider indicators */}
+          <div className="absolute z-30 flex space-x-3 -translate-x-1/2 bottom-5 left-1/2">
+            {images.map((_, index) => (
+              <button
+                key={index}
+                type="button"
+                className={`w-3 h-3 rounded-full ${
+                  index === currentSlide ? "bg-white" : "bg-gray-500"
+                }`}
+                aria-current={index === currentSlide ? "true" : "false"}
+                aria-label={`Slide ${index + 1}`}
+                onClick={() => setCurrentSlide(index)}
+              ></button>
+            ))}
+          </div>
+        </div>
+
+        {/* Right column container */}
+
+        <div className="px-4 md:px-0 lg:w-6/12">
+          <div className="md:mx-6 md:p-12">
+            {/* LOGO */}
+            <div className="logoLogin">
+              <img className="mx-auto w-9/12" src={Logo} alt="logo" />
+            </div>
+            {/* FORM */}
+            <span className="text-2xl text-secondary-300 font-medium">
+              Inicia Sesión o{" "}
+              <a
+                href="#"
+                className="text-secondary-100 hover:underline"
+                // onClick={() => navigate("/register")}
+              >
+                Registrate
+              </a>
+            </span>
+            <div className="mt-12">
+              <form>
+                <p className="loginForm mb-4">EMAIL</p>
+                <input
+                  type="email"
+                  placeholder="Escribe tu email"
+                  className="AT-input mb-8"
+                />
+                <p className="loginForm mb-4">CONTRASEÑA</p>
+                <input
+                  type="password"
+                  placeholder="Escribe tu contraseña"
+                  className="AT-input"
+                />
+              </form>
+            </div>
+          </div>
+          <div className="mt-8 md:mt-0">
+            {/* Submit button */}
+            <div className="max-w-lg flex justify-center md:justify-end items-center ml-8 md:ml-44">
+              <button
+                className="loginButton"
+                type="button"
+                data-te-ripple-init
+                data-te-ripple-color="light"
+              >
+                Entrar
+              </button>
+              <a
+                href="#"
+                className="text-secondary-300 font-medium hover:text-secondary-100 transition-colors"
+              >
+                ¿Olvidaste tu contraseña?
+              </a>
             </div>
           </div>
         </div>
