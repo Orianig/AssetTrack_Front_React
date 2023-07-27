976db4d2cc21b7dbdf733424516ad397ebf48ea8
orianig <oriana.ig016@gmail.com>
2023-07-27 12:00:03 +0200

    feat: add header structure

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <>
+     <header className="h-[7vh] md:h-[10vh] border-b border-secondary-100 p-8 flex items-center justify-end">
+
+<h1>HEADER</h1>
+     </header>
+     </>
+  )
+};
+
+export default Header;
