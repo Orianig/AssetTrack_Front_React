d7c85eeef73c4b9936a3294847de1def8ddb49c3
orianig <oriana.ig016@gmail.com>
2023-07-28 11:16:16 +0200

    refactor: delete console.logs

--- src/redux/slices/user.slice.js
+++ src/redux/slices/user.slice.js
@@ -3,7 +3,6 @@ import { createSlice } from "@reduxjs/toolkit";
 export const userSlice = createSlice({
     name: "user",
     initialState: {
-      //se mete en credentials y data para devolver el nuevo estado en estas
       credentials: {
         token: "",
       },
@@ -19,20 +18,10 @@ export const userSlice = createSlice({
     reducers: {
         login: (state, action) => {
           const { token, data } = action.payload;
-
-          console.log("Login Action Payload:", action.payload);
-      console.log("Previous State:", state);
-
           state.credentials.token = token;
           state.data = data;
-
-          console.log("Updated State:", state);
-
         },
         logout: (state) => {
-
-            console.log("Logout Action Triggered");
-      console.log("Previous State:", state); 
           state.credentials.token = "";
           state.data = {
             id: "",
@@ -41,8 +30,6 @@ export const userSlice = createSlice({
             name: "",
             surname: "",
           };
-
-          console.log("Updated State:", state);
         },
       },
     });
