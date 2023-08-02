dfded605f37c4e0b5d9ecd58d86e39c4d9376990
orianig <oriana.ig016@gmail.com>
2023-08-02 23:46:06 +0200

    feat: add button basic structure

---
+++ src/components/ButtonsProject.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const ButtonProject = () => {
+  return (
+<>
+      <button
+        type="button"
+        className="inline-block rounded bg-secondary-100 px-6 pb-2 pt-2.5 text-xs font-medium uppercase leading-normal text-primary-700 transition duration-150 ease-in-out hover:bg-primary focus:bg-primary-accent-100 focus:outline-none focus:ring-0 active:bg-primary"
+      >
+        Secondary
+      </button>
+      </>
+  );
+};
+export default ButtonProject;
