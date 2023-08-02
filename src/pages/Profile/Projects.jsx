bcc6d242550c111da3214ad15ec6350dadbdbd8b
orianig <oriana.ig016@gmail.com>
2023-08-02 21:18:00 +0200

    feat: add project card basic structure

--- src/pages/Profile/Projects.jsx
+++ src/pages/Profile/Projects.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+import CardProject from "../../components/CardProjects";
 
 const Projects = () => {
-  return <h1> SOY PROJECTS </h1>;
+  return (
+    <>
+      <h1> SOY PROJECTS </h1>
+      <CardProject />
+    </>
+  );
 };
 
 export default Projects;
