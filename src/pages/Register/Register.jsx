b49c7bda46ee1d4e903aec49f3b2da83e76434b2
orianig <oriana.ig016@gmail.com>
2023-08-02 20:54:42 +0200

    style: carousel

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
-
+import Carousel from "../../components/Carousel";
 import Logo from "../../assets/images/logo-4.png";
 import { register } from "../../services/auth.service";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
+
 
 const Register = () => {
   //HOOKS
@@ -42,8 +43,7 @@ const Register = () => {
     <>
       <div className="h-screen flex flex-col lg:flex-row">
                 {/* Left column container */}
-
-
+                <Carousel />
         {/* Right column container */}
         <div className="px-4 md:px-0 lg:w-6/12">
           <div className="md:mx-6 md:p-12">
